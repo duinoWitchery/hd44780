24782c87c51a82c52c2e0ad08412de50891abc59
Bill Perry <bperrybap@opensource.billsworld.billandterrie.com>
2016-07-22 17:41:46 -0600

    refactored to put all i/o subclass code into respective headers

--- hd44780_I2Clcd.h
+++ hd44780_I2Clcd.h
@@ -12,7 +12,7 @@
 //
 //  hd44780_I2Clcd is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License.
+//  the Free Software Foundation version 3 of the License.
 //
 //  hd44780_I2Clcd is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -61,12 +61,13 @@
 // Attempting to read from some of these devices will lockup the AVR Wire
 // library.
 //
+// 2017.07.21  bperrybap - merged all class code into header
 // 2017.07.20  bperrybap - merged into hd44780 library
 // 2016.06.09  bperrybap - changed name to hd44780_I2Clcd
 // 2016.06.08  bperrybap - removed pre IDE 1.0 support
 // 2016.06.03  bperrybap - added smart execution delays
 // 2014.02.15  bperrybap - changed to use hd44780 base class library
-// 2013.11.15  bperrybap - initial creation (hd44780_IIClcd)
+// 2013.06.01  bperrybap - initial creation (hd44780_IIClcd)
 //
 // @author Bill Perry - bperrybap@opensource.billsworld.billandterrie.com
 //
@@ -79,24 +80,113 @@
 class hd44780_I2Clcd : public hd44780 
 {
 public:
-	hd44780_I2Clcd(uint8_t  i2c_addr);
+// ====================
+// === constructors ===
+// ====================
+
+hd44780_I2Clcd(uint8_t i2c_addr)
+{
+	_Addr = i2c_addr; // Save away the device address in object
+}
+
+
 private:
-	// Control byte bit values
+// ====================
+// === private data ===
+// ====================
 
- 	// RS signal in HD44780 interface
-	// set for data writes, clear for cmds
-	// note: some datasheets call this A0 bit
-	static const uint8_t I2Clcd_RS  = (1 << 6);
+// Control byte bit values
 
-	// set if another control byte following data
-	// clear if last control byte
-	static const uint8_t  I2Clcd_CO = (1 << 7);
+// RS signal in HD44780 interface
+// set for data writes, clear for cmds
+// note: some datasheets call this A0 bit
+static const uint8_t I2Clcd_RS  = (1 << 6);
 
+// set if another control byte following data
+// clear if last control byte
+static const uint8_t  I2Clcd_CO = (1 << 7);
 
-	// hd44780 class virtual i/o functions
-	int  ioinit();
-	void iosend(uint8_t value, hd44780::iosendtype type);
+uint8_t _Addr;             // I2C Address of the LCD
 
-	uint8_t _Addr;             // I2C Address of the LCD
-};
+// ==================================================
+// === hd44780 i/o subclass virtual i/o functions ===
+// ==================================================
+
+// ioinit() - Returns non zero if initialization failed.
+int ioinit()
+{
+int status = 0;
+
+	/*
+	 * First, initialize the i2c (Wire) library.
+	 * This really shouldn't be here 
+	 * because Wire.begin() should only be called once, but
+	 * unfortunately, there is no way to know if anybody
+	 * else has called this.
+	 * I believe that it is unreasonable to require the the user
+	 * sketch code to do it, because all that should change between
+	 * interfaces should be the constructor
+	 * So we go ahead and call it here.
+	 */
+	Wire.begin();
+
+	/*
+	 * Check to see if the device is responding
+	 */
+	Wire.beginTransmission(_Addr);
+	status = Wire.endTransmission();
+
+	/*
+	 * this device only runs in 8 bit mode
+	 */
+	_displayfunction = HD44780_8BITMODE;
+   return ( status );
+}
+
+//
+// iosend() - send either a command or data byte to lcd
+void iosend(uint8_t value, iosendtype type) 
+{
+uint8_t ctlbyte;
+   
+	/*
+	 * No need to look for 4 bit commands as all bits are already in
+	 * proper upper nibble and unsued bits are zero.
+	 * This allows the special 4 bit commands to be sent "As is" when
+	 * using an 8 bit interface - which is what this i2c LCD uses.
+	 */
+
+	if(type == hd44780::HD44780_IOdata)
+	{
+		ctlbyte = I2Clcd_RS; // control byte with RS and no continue
+	}
+	else
+	{
+		ctlbyte = 0; // control byte with no RS and no continue
+	}
+
+	
+	/*
+	 * ensure that previous LCD instruction finished.
+	 * There is a 25us offset since there will be at least 1 byte
+	 * (the i2c address)  transmitted over i2c before the next LCD instruction
+	 * information could get to the LCD.
+	 * At 400Khz (max rate supported by the LCD) 8 bits plus start and stop
+	 * bits is 25us.
+	 * So there is at least 25us of time overhead in the physical interface.
+	 */
+
+	waitReady(-25);
+
+	/*
+	 * Send the next LCD instruction
+	 */
+
+	Wire.beginTransmission(_Addr);
+	Wire.write(ctlbyte);	// send control byte
+	Wire.write(value);		// send data/cmd
+	Wire.endTransmission();
+}
+
+}; // end of class definition
 #endif
--- hd44780_pinIO.h
+++ hd44780_pinIO.h
@@ -29,7 +29,9 @@
 // with the functionality of the Arduino LiquidCrystal library.
 //
 //
+// 2016.07.21  bperrybap - merged all class code into header
 // 2016.07.20  bperrybap - merged into hd44780 library
+// 2016.06.03  bperrybap - added smart execution delays
 // 2016.04.01  bperrybap - initial creation
 //
 // @author Bill Perry - bperrybap@opensource.billsworld.billandterrie.com
@@ -43,33 +45,188 @@
 #ifndef hd44780_h
 #error Missing hd44780.h header file
 #endif
+#include <pins_arduino.h> // to get PWM detection macros
 
 class hd44780_pinIO : public hd44780
 {
 public:
-   // 4 bit mode constructor with optional backlight control
-   hd44780_pinIO(uint8_t rs, uint8_t en,
-                         uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
-	                     uint8_t bl=0xff, uint8_t blLevel=0xff); // optional
+// ====================
+// === constructors ===
+// ====================
+
+// 4 bit mode constructor with optional backlight control
+hd44780_pinIO(uint8_t rs,  uint8_t en,
+			uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
+			uint8_t bl=0xff, uint8_t blLevel=0xff)
+{
+	_rs = rs;
+	_en = en;
+	_d4 = d4;
+	_d5 = d5;
+	_d6 = d6;
+	_d7 = d7;
+	_bl = bl;
+	_blLevel = blLevel;
+}
+
+
 private:
-	// hd44780 class virtual i/o functions
-	int  ioinit();
-	void iosend(uint8_t value, hd44780::iosendtype type);
-	void iosetBacklight (uint8_t dimvalue);
+// ====================
+// === private data ===
+// ====================
 
-	// internal functions
-	void write4bits(uint8_t value);
-	void pulseEnable();
+// Arduino Pin information
+uint8_t _rs;		// hd44780 rs arduino pin
+uint8_t _rw;		// hd44780 rw arduino pin
+uint8_t _en;		// hd44780 en arduino pin
+uint8_t _d4;		// hd44780 d4 arduino pin
+uint8_t _d5;		// hd44780 d4 arduino pin
+uint8_t _d6;		// hd44780 d4 arduino pin
+uint8_t _d7;		// hd44780 d4 arduino pin
+uint8_t _bl;		// arduino pin to control backlight
+uint8_t _blLevel;	// backlight active control level HIGH/LOW
 
-	// Arduino Pin information
-	uint8_t _rs;		// hd44780 rs arduino pin
-	uint8_t _rw;		// hd44780 rw arduino pin
-	uint8_t _en;		// hd44780 en arduino pin
-	uint8_t _d4;		// hd44780 d4 arduino pin
-	uint8_t _d5;		// hd44780 d4 arduino pin
-	uint8_t _d6;		// hd44780 d4 arduino pin
-	uint8_t _d7;		// hd44780 d4 arduino pin
-	uint8_t _bl;		// arduino pin to control backlight
-	uint8_t _blLevel;	// backlight active control level HIGH/LOW
-};
+
+// ==================================================
+// === hd44780 i/o subclass virtual i/o functions ===
+// ==================================================
+
+// ioinit() - initialize the h/w
+int ioinit()
+{
+	// initialize Arduino pins used for hd44780 signals
+	pinMode(_rs, OUTPUT);
+	pinMode(_en, OUTPUT);
+	pinMode(_d4, OUTPUT);
+	pinMode(_d5, OUTPUT);
+	pinMode(_d6, OUTPUT);
+	pinMode(_d7, OUTPUT);
+  
+	if(_bl != 0xff)
+		pinMode(_bl, OUTPUT);
+
+	return(0); // all is good
+}
+// iosend() - send either a command or data byte to lcd
+void iosend(uint8_t value, hd44780::iosendtype type)
+{
+	if(type == hd44780::HD44780_IOdata)
+  		digitalWrite(_rs, HIGH);
+	else
+  		digitalWrite(_rs, LOW);
+  
+	// "4 bit commands" are special.
+	// They are used only during initalization and
+	// are used to reliably get the LCD and host in nibble sync
+	// with each other and to get the LCD into 4 bit mode.
+	// 8 bit host interfaces don't have to worry about this,
+	// but 4 bit host interfaces must only send the upper nibble.
+
+	// note:
+	// waitReady() is explicitly called as late as possible *after* the Arduino
+	// pins for the LCD control and data signals are set to allow
+	// overhead of the digitalWrite() calls to be hidden under execution time.
+	
+	write4bits(value>>4);	// setup uppper nibble d4-d7 lcd pins
+	waitReady();			// ensure previous instruction finished
+	pulseEnable();			// send upper nibble to LCD
+
+	// send lower nibble if not a 4 bit command
+	// (sends nibble for both data and "normal" commands)
+	if (type != hd44780::HD44780_IOcmd4bit )
+	{
+		write4bits((value & 0x0F));// setup lower nibble on d4-d7 lcd pins
+		pulseEnable();				// send lower nibble to LCD
+	}
+}
+
+// iosetBacklight() - set backlight brightness
+// if dimming not supported, any non zero dimvalue turns backlight on
+void iosetBacklight(uint8_t dimvalue)
+{
+	if (_bl == 0xff )
+		return; // no backlight pin so nothing to do
+
+	// Check if backlight pin has PWM support
+	// The reason this has to be done is that Arduino analogWrite()
+	// sets a pin to LOW if there is no PWM support and the PWM value is
+	// less than half of maximum. This is not desired behavior.
+	// The desired behavior is that if dimming is not supported, then the
+	// output is full on until the PWM value is zero to indicate "off".
+	// So we have to bypass the goofy code in analogWrite()
+	// by detecting if the pin supports PWM and controlling the output manually
+	// when no PWM is available.
+
+#if defined(digitalPinHasPWM)
+   	// Newer 1.5x Arduino has a macro to check for PWM capability on a pin
+	if(digitalPinHasPWM(_bl))
+#elif defined(digitalPinToTimer)
+	// On 1.x Arduino have to check differently
+	if(digitalPinToTimer(_bl) != NOT_ON_TIMER)
+#else 
+	if(0) // no way to tell so assume no PWM
+#endif
+	{
+		// set PWM signal appropriately for active level
+		if(_blLevel == HIGH)
+		{
+			analogWrite(_bl, dimvalue);
+		}
+		else 
+		{
+			analogWrite(_bl, 255 - dimvalue); // active low is inverse PWM
+		}
+	}
+
+	// No PWM support on pin, so
+	// dimvalue 0 is off, any other value is on
+	else if(((dimvalue) && (_blLevel == HIGH)) ||
+			((dimvalue == 0) && (_blLevel == LOW)))
+	{
+		digitalWrite(_bl, HIGH);
+	}
+	else
+	{
+		digitalWrite(_bl, LOW);
+	}
+}
+
+// ================================
+// === internal class functions ===
+// ================================
+
+// write4bits() - set the 4 hd44780 data lines
+void write4bits(uint8_t value)
+{
+	// write the bits on the LCD data lines but don't send the data
+	if(value & 1)
+		digitalWrite(_d4, HIGH);
+	else
+		digitalWrite(_d4, LOW);
+
+	if(value & 2)
+		digitalWrite(_d5, HIGH);
+	else
+		digitalWrite(_d5, LOW);
+
+	if(value & 4)
+		digitalWrite(_d6, HIGH);
+	else
+		digitalWrite(_d6, LOW);
+
+	if(value & 8)
+		digitalWrite(_d7, HIGH);
+	else
+		digitalWrite(_d7, LOW);
+}
+
+// pulseEnable() - toggle en to send data to hd44780 module
+void pulseEnable(void)
+{
+	digitalWrite(_en, HIGH);
+	delayMicroseconds(1);    // enable pulse must be >450ns
+	digitalWrite(_en, LOW);
+}
+
+}; // end of class definition
 #endif
