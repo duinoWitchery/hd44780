56013859e9c381256d4d60c132f6c7b97ca6929b
Bill Perry <bperrybap@opensource.billsworld.billandterrie.com>
2018-02-11 15:15:29 -0600

    Documenation sketch updates

--- examples/Documentation/Documentation.ino
+++ examples/Documentation/Documentation.ino
@@ -1,10 +1,74 @@
 ///////////////////////////////////////////////////////////////////////////////
 // hd44780 Extensible hd44780 LCD Library Version 0.9.2
 //
+// hd44780 is an open source project for open source projects. Those wishing to
+// create closed projects should seek an alternate solution.
+// hd44780 is licensed under the terms of the GNU General Public License v3.0
+// as opposed to the more liberal licensing terms of the
+// GNU Lesser General Public License (LGPL), MIT, modified BSD, Apache, etc..
+// 
+// GPL licensing information can found here: https://www.gnu.org/licenses/
+///////////////////////////////////////////////////////////////////////////////
+//
 // hd44780 Library ReadMe: file://./docs/README.html
 //
-// hd44780 github respository: https://github.com/duinoWitchery/hd44780
-// (note: repository may be different version from installed version)
+// Overview
+// ========
+// The hd44780 arduino library is more than a simple library to run a specific
+// h/w. It is actually an expandable library package that can communicate with
+// many different types of LCD hardware and interfaces.
+//
+// The library is designed to allow communication and interfacing with hd44780
+// LCDs over a variety of h/w interfaces.
+// Each h/w communication interface has its own C++ i/o class.
+//
+// The library currently comes with the following i/o subclasses:
+//
+//    hd44780_pinIO: control LCD using direct Arduino Pin connections
+//    hd44780_I2Cexp: control LCD using i2c i/o exapander backpack (PCF8574 or MCP23008)
+//    hd44780_I2Clcd: control LCD with native i2c interface (PCF2116, PCF2119x, etc...)
+//    hd44780_NTCU165ECPB: control Noritake CU165ECBP-T2J LCD display over SPI
+//    hd44780_NTCUUserial: control Noritake CU-U Series VFD display in serial mode
+//
+// Examples
+// ========
+// hd44780examples:
+// These are not really example sketches. They should not be looked at as
+// examples for how to use the library. They are special purpose sketches
+// that are used as include files for the i/o class examples.
+// See the ioClass specific examples for each ioClass.
+//
+// ioClass: 
+//    Contains subdirectories for each included hd44780 i/o class containing
+//    sketches that are specific to the i/o class.
+//    The examples for each i/o class are grouped together in a directory by
+//    the name of the i/o class.
+//    In the Arduino IDE they can be found here:
+//        [File]->Examples->hd44780->ioClass
+//    For example, the examples for the hd44780_I2Cexp i/o class will be
+//    found here:
+//        [File]->Examples->hd44780->ioClass->hd44780_I2Cexp
+//
+// otherLibraries:
+//     Contains subdirectories for other (non hd44780) libraries that contain
+//     wrapper sketches for various hd44780examples sketches. This is intended
+//     to allow easy benchmarking of other libraries for comparison purposes.
+//
+// See the readme files under the examples directory and the wiki for
+// additional information about the examples.
+//
+// Custom Character creation tools
+// ===============================
+//    https://kakedev.github.io/GlyphGenerator/
+//    http://www.quinapalus.com/hd44780udg.html
+//    https://omerk.github.io/lcdchargen
+//
+// Github links
+// ============
+// Version 0.9.2 https://github.com/duinoWitchery/hd44780/tree/0.9.2
+// Latest respository: https://github.com/duinoWitchery/hd44780
+// hd44780 wiki: https://github.com/duinoWitchery/hd44780/wiki
+// (note: Latest repository & wiki may be different from installed version)
 //
 // Arduino Links
 // =============
@@ -21,12 +85,20 @@
 //    http://fab.cba.mit.edu/classes/863.06/11.13/44780.pdf
 //    https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
 //
+// hd44780 internal RAM addressing:
+//    http://web.alfredstate.edu/faculty/weimandn/lcd/lcd_addressing/lcd_addressing_index.html
+//    https://arduino-info.wikispaces.com/file/view/LCD+Addressing.pdf
+//
 // Arduino board Pinouts:
-//    https://github.com/Bouni/Arduino-Pinout
-//    https://www.pjrc.com/teensy/pinout.html
-//    https://iotbytes.wordpress.com/esp8266-pinouts/
-//    https://iotbytes.wordpress.com/nodemcu-pinout/
-//    https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/pinouts
+//    Various Popular Arduino boards:
+//      http://www.pighixxx.com/test/pinoutspg/boards/
+//      https://github.com/Bouni/Arduino-Pinout
+//    Teensy boards:
+//      https://www.pjrc.com/teensy/pinout.html
+//    Various esp8266 based boards:
+//      https://iotbytes.wordpress.com/esp8266-pinouts/
+//      https://iotbytes.wordpress.com/nodemcu-pinout/
+//      https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/pinouts
 //
 // PCF8574 datasheet:
 //    https://www.aurel32.net/elec/pcf8574.pdf
